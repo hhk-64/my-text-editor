7fa1f74e76929dc59c0b4e1d1577fff0459d1a9d
halil-kozak64 <h.kozak64@hotmail.com>
2022-09-14 00:53:16 +0300

    Added MenuBar and New File Action done

--- function.py
+++ function.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import *
-from PySide6.QtCore import Qt, Slot
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+import os
 
 
 class MainWindow(QWidget):
@@ -7,5 +9,37 @@ class MainWindow(QWidget):
 	def __init__(self):
 		super().__init__()
 
-		self.setFixedSize(720, 480)
+		self.setMinimumSize(720, 480)
 		self.setWindowTitle("Text Editor")
+		self.setWindowIcon(self.style().standardIcon(getattr(QStyle, "SP_FileIcon")))
+
+		self.layout = QHBoxLayout()
+
+
+		self.menuBar = QMenuBar(self)
+		self.FileMenu = self.menuBar.addMenu("File")
+		self.FileMenu.NewFileAction = self.FileMenu.addAction("New File...")
+		self.FileMenu.OpenFileAction = self.FileMenu.addAction("Open File...")
+		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
+
+
+		self.TextArea = QPlainTextEdit(self)
+		self.TextArea.move(QPoint(0, 0+self.menuBar.height()))
+		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
+
+
+		self.FileMenu.NewFileAction.triggered.connect(self.CreateFile)
+	
+
+
+	def resizeEvent(self, event):
+		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
+		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
+		return super(MainWindow, self).resizeEvent(event)
+	
+
+	@Slot()
+	def CreateFile(self):
+		f = QFileDialog.getSaveFileName(self, "Create a new file")[0]
+		with open(f, "w", encoding="UTF-8") as f:
+			pass

fa49aea1a36ecf9018b62ee9b33f7ab25c057758
halil-kozak64 <h.kozak64@hotmail.com>
2022-09-22 22:58:38 +0300

    Added functionality

--- function.py
+++ function.py
@@ -15,20 +15,30 @@ class MainWindow(QWidget):
 
 		self.layout = QHBoxLayout()
 
+		self.textBuffer = ["", False]
+
 
 		self.menuBar = QMenuBar(self)
+
 		self.FileMenu = self.menuBar.addMenu("File")
 		self.FileMenu.NewFileAction = self.FileMenu.addAction("New File...")
 		self.FileMenu.OpenFileAction = self.FileMenu.addAction("Open File...")
+		self.FileMenu.addSeparator()
+
+		self.FileMenu.SaveAction = self.FileMenu.addAction("Save")
+		self.FileMenu.SaveAction.setDisabled(True)
+
 		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
 
 
 		self.TextArea = QPlainTextEdit(self)
 		self.TextArea.move(QPoint(0, 0+self.menuBar.height()))
 		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
+		self.TextArea.textChanged.connect(self.CheckSaveBuffer)
 
 
 		self.FileMenu.NewFileAction.triggered.connect(self.CreateFile)
+		self.FileMenu.OpenFileAction.triggered.connect(self.OpenFile)
 	
 
 
@@ -37,9 +47,31 @@ class MainWindow(QWidget):
 		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
 		return super(MainWindow, self).resizeEvent(event)
 	
+	def textChanged(self):
+		if self.TextArea.toPlainText() != self.textBuffer: self.FileMenu.SaveAction.setDisabled(False)
+		elif self.TextArea.toPlainText() == self.textBuffer: self.FileMenu.SaveAction.setDisabled(True)
+	
 
 	@Slot()
 	def CreateFile(self):
-		f = QFileDialog.getSaveFileName(self, "Create a new file")[0]
-		with open(f, "w", encoding="UTF-8") as f:
-			pass
+		f = QFileDialog.getSaveFileName(self, "Create a new file", filter="Text Files (*.txt)")[0]
+		if f != "":
+			with open(f, "w", encoding="UTF-8") as fl:
+				pass
+	
+	@Slot()
+	def OpenFile(self):
+		f = QFileDialog.getOpenFileName(self, "Open a file", filter="Text Files (*.txt)")[0]
+		if f != "":
+			with open(f, "r", encoding="UTF-8") as fl:
+				content = fl.read()
+				self.TextArea.setPlainText(content)
+				self.TextArea.moveCursor(QTextCursor.End, QTextCursor.MoveAnchor)
+				self.textBuffer = content, True
+	
+	@Slot()
+	def CheckSaveBuffer(self):
+		if self.textBuffer[1] == False: return
+		
+		if self.TextArea.toPlainText() != self.textBuffer: self.FileMenu.SaveAction.setDisabled(False)
+		elif self.TextArea.toPlainText() == self.textBuffer: self.FileMenu.SaveAction.setDisabled(True)

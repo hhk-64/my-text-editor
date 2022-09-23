f5e88e6ba03ea843f47b6db6f96201f7681d6c22
halil-kozak64 <h.kozak64@hotmail.com>
2022-09-23 03:32:30 +0300

    V1.0

--- function.py
+++ function.py
@@ -16,48 +16,62 @@ class MainWindow(QWidget):
 		self.layout = QHBoxLayout()
 
 		self.textBuffer = ["", False]
+		self.curFile = ""
 
 
 		self.menuBar = QMenuBar(self)
 
 		self.FileMenu = self.menuBar.addMenu("File")
+
 		self.FileMenu.NewFileAction = self.FileMenu.addAction("New File...")
+		self.FileMenu.NewFileAction.triggered.connect(self.CreateFile)
+
 		self.FileMenu.OpenFileAction = self.FileMenu.addAction("Open File...")
+		self.FileMenu.OpenFileAction.triggered.connect(self.OpenFile)
+
 		self.FileMenu.addSeparator()
 
 		self.FileMenu.SaveAction = self.FileMenu.addAction("Save")
+		self.FileMenu.SaveAction.triggered.connect(self.SaveFile)
 		self.FileMenu.SaveAction.setDisabled(True)
+
+		self.FileMenu.SaveAsAction = self.FileMenu.addAction("Save As...")
+		self.FileMenu.SaveAsAction.triggered.connect(self.SaveFileAs)
 
 		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
 
 
+		self.curFileDisplay = QLabel("No File opened", self)
+		self.curFileDisplay.setStyleSheet("padding-left: 2px")
+		self.curFileDisplay.setFixedSize(self.width(), 25)
+		self.curFileDisplay.move(0, self.height()-self.curFileDisplay.sizeHint().height())
+
+
 		self.TextArea = QPlainTextEdit(self)
 		self.TextArea.move(QPoint(0, 0+self.menuBar.height()))
-		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
+		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height()-self.curFileDisplay.height())
 		self.TextArea.textChanged.connect(self.CheckSaveBuffer)
-
-
-		self.FileMenu.NewFileAction.triggered.connect(self.CreateFile)
-		self.FileMenu.OpenFileAction.triggered.connect(self.OpenFile)
 	
 
 
 	def resizeEvent(self, event):
 		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
-		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
+		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height()-self.curFileDisplay.height())
+		self.curFileDisplay.setFixedSize(self.width(), 25)
+		self.curFileDisplay.move(0, self.height()-self.curFileDisplay.sizeHint().height())
 		return super(MainWindow, self).resizeEvent(event)
 	
-	def textChanged(self):
-		if self.TextArea.toPlainText() != self.textBuffer: self.FileMenu.SaveAction.setDisabled(False)
-		elif self.TextArea.toPlainText() == self.textBuffer: self.FileMenu.SaveAction.setDisabled(True)
-	
 
+	
 	@Slot()
 	def CreateFile(self):
 		f = QFileDialog.getSaveFileName(self, "Create a new file", filter="Text Files (*.txt)")[0]
-		if f != "":
-			with open(f, "w", encoding="UTF-8") as fl:
-				pass
+		if f == "": return
+		
+		with open(f, "w", encoding="UTF-8") as fl:
+			self.textBuffer = ["", True]
+			self.curFile = f
+			self.curFileDisplay.setText(self.curFile)
 	
 	@Slot()
 	def OpenFile(self):
@@ -66,12 +80,35 @@ class MainWindow(QWidget):
 			with open(f, "r", encoding="UTF-8") as fl:
 				content = fl.read()
 				self.TextArea.setPlainText(content)
-				self.TextArea.moveCursor(QTextCursor.End, QTextCursor.MoveAnchor)
-				self.textBuffer = content, True
+				self.textBuffer = [content, True]
+				self.curFile = f
+				self.curFileDisplay.setText(self.curFile)
 	
 	@Slot()
 	def CheckSaveBuffer(self):
 		if self.textBuffer[1] == False: return
 		
-		if self.TextArea.toPlainText() != self.textBuffer: self.FileMenu.SaveAction.setDisabled(False)
-		elif self.TextArea.toPlainText() == self.textBuffer: self.FileMenu.SaveAction.setDisabled(True)
+		if self.TextArea.toPlainText() != self.textBuffer[0]: self.FileMenu.SaveAction.setDisabled(False)
+		elif self.TextArea.toPlainText() == self.textBuffer[0]: self.FileMenu.SaveAction.setDisabled(True)
+	
+	@Slot()
+	def SaveFile(self):
+		if self.curFile == "": return
+
+		with open(self.curFile, "w", encoding="UTF-8") as fl:
+			content = self.TextArea.toPlainText()
+			fl.write(content)
+			self.textBuffer[0] = content
+			self.FileMenu.SaveAction.setDisabled(True)
+	
+	@Slot()
+	def SaveFileAs(self):
+		f = QFileDialog.getSaveFileName(self, "Save File As", filter="Text Files (*.txt)")[0]
+		if f == "": return
+		
+		with open(f, "w", encoding="UTF-8") as fl:
+			content = self.TextArea.toPlainText()
+			fl.write(content)
+			self.textBuffer = [content, True]
+			self.curFile = f
+			self.curFileDisplay.setText(self.curFile)

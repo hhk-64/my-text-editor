499b8c8cea5dc2619f78ce69e4487a501e0bf4e7
halil-kozak64 <h.kozak64@hotmail.com>
2022-10-24 21:44:05 +0300

    V2.0 Release

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # A Text Editor I made
 
+### V2.0 Released | First Major Update | Changelog (Oct. 24th)
+
+- Improved performance and code readability
+- Removed "Change Color" and "Change Background Color" settings <br>
+	⤷ Instead added default "dark themed" colors
+- Added "Minimize" and "Exit" Button
+- Removed Windows Toolbar and added custom toolbar to match the dark theme
+
 ### V1.3 Released | Changelog (Sep. 28th)
 
 - Fixed some performance bugs
--- function.py
+++ function.py
@@ -1,9 +1,83 @@
+#	----- CUSTOM COLORS -----	#
+BACKGROUND_COLOR = "#424242"
+BORDER_COLOR = "#212121"
+PRIMARY_COLOR = "#616161"
+SECONDARY_COLOR = "#BDBDBD"
+HOVER_COLOR = "#212121"
+#	-------------------------	#
+
+
+
 from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
-import os, json
+import os, json, functools
 
-from shiboken6 import isValid
+
+
+
+
+class FunctionButton(QPushButton):
+
+	def __init__(self, parent, txt, endColor):
+
+		super().__init__()
+		self.setParent(parent)
+		self.setText(txt)
+
+		self._curColor = QColor(PRIMARY_COLOR)
+
+		self.initEndColor = QColor(endColor)
+
+		self.endColor = QColor(PRIMARY_COLOR)
+	
+	def helper_function(self, color):
+
+		self.setStyleSheet(f"background-color: {color.name()};color: {SECONDARY_COLOR}; font-size: 16px; border: none;")
+
+		self._curColor = color
+	
+	def apply_color_animation(self, start_color, duration=1000):
+		animation = QVariantAnimation(self)
+		animation.setStartValue(start_color)
+		animation.setEndValue(self.endColor)
+		animation.setDuration(duration)
+		animation.setEasingCurve(QEasingCurve.InCurve)
+
+		animation.valueChanged.connect(functools.partial(self.helper_function))
+		animation.start(QAbstractAnimation.DeleteWhenStopped)
+	
+
+	def enterEvent(self, event):
+		self.endColor = QColor(self.initEndColor)
+		self.apply_color_animation(self._curColor, 100)
+		return super().enterEvent(event)
+	
+	def leaveEvent(self, event):
+		self.endColor = QColor(PRIMARY_COLOR)
+		self.apply_color_animation(self._curColor, 100)
+		return super().leaveEvent(event)
+
+
+
+
+
+class ExitDialog(QMessageBox):
+
+	def __init__(self, txt = "You have unsaved changes!"):
+		
+		super().__init__()
+		
+		self.setText(txt)
+		self.setInformativeText("Do you want to save your changes to the file?")
+		self.saveButton = self.addButton("Save", QMessageBox.AcceptRole)
+		self.setDefaultButton(self.saveButton)
+		self.dontSaveButton = self.addButton("Don't save", QMessageBox.DestructiveRole)
+		self.cancelButton = self.addButton("Cancel", QMessageBox.RejectRole)
+		self.setEscapeButton(self.cancelButton)
+
+
+
 
 
 class MainWindow(QWidget):
@@ -14,25 +88,37 @@ class MainWindow(QWidget):
 		self.setMinimumSize(720, 480)
 		self.setWindowTitle("Text Editor")
 		self.setWindowIcon(self.style().standardIcon(getattr(QStyle, "SP_FileIcon")))
+		self.setStyleSheet(f"background-color: {PRIMARY_COLOR}")
 
-		self.layout = QHBoxLayout()
-
-
+		self.setWindowFlags(Qt.FramelessWindowHint | Qt.CustomizeWindowHint)
+		
+		
 		if not ReadJsonFile(): self.settingsData = CreateJsonFile()
 		else: self.settingsData = ReadJsonFile()
 
 		self.font_ = QFont()
 		self.font_.fromString(self.settingsData[0]["font"])
 
-		self.color_ = self.settingsData[0]["color"]
-		self.colorBG_ = self.settingsData[0]["background"]
-
 
 		self.textBuffer = ["", False]
 		self.curFile = ""
 
-
+		self.start = QPoint(0, 0)
+		self.pressing = False
+		
+		
 		self.menuBar = QMenuBar(self)
+
+		self.menuBar.setStyleSheet(f"""
+									QMenuBar {{
+										background-color: {PRIMARY_COLOR};
+										color: {SECONDARY_COLOR};
+										font-size: 16px;
+									}}
+									QMenuBar::item:selected {{
+										background-color: {HOVER_COLOR};
+									}}
+									""")
 
 		self.menuBar.FileMenu = self.menuBar.addMenu("File")
 
@@ -50,9 +136,9 @@ class MainWindow(QWidget):
 
 		self.menuBar.FileMenu.addSeparator()
 
-		self.menuBar.FileMenu.SaveAction = self.menuBar.FileMenu.addAction("Save", QKeySequence("Ctrl+S"))
-		self.menuBar.FileMenu.SaveAction.triggered.connect(self.SaveFile)
-		self.menuBar.FileMenu.SaveAction.setDisabled(True)
+		self.menuBar.FileMenu.SaveFileAction = self.menuBar.FileMenu.addAction("Save", QKeySequence("Ctrl+S"))
+		self.menuBar.FileMenu.SaveFileAction.triggered.connect(self.SaveFile)
+		self.menuBar.FileMenu.SaveFileAction.setDisabled(True)
 
 		self.menuBar.FileMenu.SaveAsAction = self.menuBar.FileMenu.addAction("Save As...", QKeySequence("Ctrl+Shift+S"))
 		self.menuBar.FileMenu.SaveAsAction.triggered.connect(self.SaveFileAs)
@@ -62,61 +148,130 @@ class MainWindow(QWidget):
 		self.menuBar.FileMenu.ExitAction = self.menuBar.FileMenu.addAction("Exit", QKeySequence("Ctrl+Shift+Q"))
 		self.menuBar.FileMenu.ExitAction.triggered.connect(self.ExitProgram)
 
-		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
+		self.menuBar.FileMenu.setStyleSheet(f"""
+											QMenu {{
+												background-color: {PRIMARY_COLOR};
+												color: {SECONDARY_COLOR};
+											}}
+											QMenu::item:selected {{
+												background-color: {HOVER_COLOR};
+											}}
+											QMenu::item:disabled {{
+												background-color: {PRIMARY_COLOR};
+												color: black;
+											}}
+											""")
 
 
 		self.menuBar.SettingsMenu = self.menuBar.addMenu("Settings")
 		
 		self.menuBar.SettingsMenu.ChangeFontAction = self.menuBar.SettingsMenu.addAction("Change Font...")
 		self.menuBar.SettingsMenu.ChangeFontAction.triggered.connect(self.changeFont)
-
-		self.menuBar.SettingsMenu.ChangeColorAction = self.menuBar.SettingsMenu.addAction("Change Color...")
-		self.menuBar.SettingsMenu.ChangeColorAction.triggered.connect(self.changeColor)
-
-		self.menuBar.SettingsMenu.ChangeBGColorAction = self.menuBar.SettingsMenu.addAction("Change Background Color...")
-		self.menuBar.SettingsMenu.ChangeBGColorAction.triggered.connect(self.changeBGColor)
+		self.menuBar.SettingsMenu.setStyleSheet(f"""
+												QMenu {{
+													background-color: {PRIMARY_COLOR};
+													color: {SECONDARY_COLOR};
+												}}
+												QMenu::item:selected {{
+													background-color: {HOVER_COLOR};
+												}}
+												""")
 		
-		self.curFileDisplay = QLabel("No File opened", self)
-		self.curFileDisplay.setStyleSheet("padding-left: 2px")
-		self.curFileDisplay.setFixedSize(self.width(), 25)
-		self.curFileDisplay.move(0, self.height()-self.curFileDisplay.sizeHint().height())
+		self.menuBar.setFixedSize(self.menuBar.sizeHint())
 
 
 		self.TextArea = QTextEdit(self)
-		self.TextArea.move(QPoint(0, 0+self.menuBar.height()))
-		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height()-self.curFileDisplay.height())
 		self.TextArea.setFont(self.font_)
-		self.TextArea.setStyleSheet(f"color: {self.color_}; background-color: {self.colorBG_};")
+		self.TextArea.setStyleSheet(f"""
+									background-color: {BACKGROUND_COLOR};
+									border: 1px solid {BORDER_COLOR};
+									color: white;
+									""")
 		self.TextArea.textChanged.connect(self.CheckSaveBuffer)
+
+
+		self.curFileDisplay = QLabel("No File opened")
+		self.statusBar = QStatusBar(self)
+		self.statusBar.addWidget(self.curFileDisplay)
+		self.statusBar.setStyleSheet(f"""
+									QStatusBar {{
+										background-color: {PRIMARY_COLOR};
+									}}
+									QStatusBar::item {{
+										border: none;
+									}}
+									QStatusBar QLabel {{
+										color: {SECONDARY_COLOR};
+									}}
+									""")
+		
+
+		self.closeButton = FunctionButton(self, "X", "red")
+		self.closeButton.setFixedSize(50, self.menuBar.sizeHint().height())
+		self.closeButton.setStyleSheet(f"""
+										QPushButton {{
+											color: {SECONDARY_COLOR};
+											border: none;
+											font-size: 16px;
+										}}
+										""")
+		self.closeButton.clicked.connect(self.triggerClose)
+
+		self.minButton = FunctionButton(self, "─", HOVER_COLOR)
+		self.minButton.setFixedSize(50, self.menuBar.sizeHint().height())
+		self.minButton.setStyleSheet(f"""
+										QPushButton {{
+											color: {SECONDARY_COLOR};
+											border: none;
+											font-size: 16px;
+										}}
+										""")
+		self.minButton.clicked.connect(self.triggerMin)
 	
 
-
+	
+	def mousePressEvent(self, event):
+		self.start = self.mapToGlobal(event.pos())
+		self.pressing = True
+	
+	def mouseMoveEvent(self, event):
+		if self.pressing:
+			if self.isMaximized():
+				return
+			self.end = self.mapToGlobal(event.pos())
+			self.movement = self.end - self.start
+			self.move(self.mapToGlobal(self.movement))
+			self.start = self.end
+	
+	def mouseReleaseEvent(self, event):
+		self.pressing = False
+	
 	def resizeEvent(self, event):
-		self.menuBar.setFixedSize(self.width(), self.menuBar.sizeHint().height())
-		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height()-self.curFileDisplay.height())
-		self.curFileDisplay.setFixedSize(self.width(), 25)
-		self.curFileDisplay.move(0, self.height()-self.curFileDisplay.sizeHint().height())
+		self.TextArea.setFixedSize(self.width(), self.height()-self.menuBar.height())
+		self.TextArea.move(0, self.menuBar.height())
+		self.statusBar.setFixedSize(self.width(), self.statusBar.sizeHint().height())
+		self.statusBar.move(0, self.height()-self.statusBar.height())
+
+		self.closeButton.move(self.width()-self.closeButton.width(), 0)
+		self.minButton.move(self.width()-self.closeButton.width()-self.minButton.width(), 0)
+
 		return super(MainWindow, self).resizeEvent(event)
 	
 	def closeEvent(self, event):
 		if self.TextArea.toPlainText() == self.textBuffer[0]:
 			self.close()
 			return
-		savePrompt = QMessageBox()
-		savePrompt.setText("Changes to the file have not been saved!")
-		savePrompt.setInformativeText("Do you want to save your changes to the file?")
-		savePrompt.saveButton = savePrompt.addButton("Save", QMessageBox.AcceptRole)
-		savePrompt.setDefaultButton(savePrompt.saveButton)
-		savePrompt.dontSaveButton = savePrompt.addButton("Don't save", QMessageBox.DestructiveRole)
-		savePrompt.cancelButton = savePrompt.addButton("Cancel", QMessageBox.RejectRole)
-		savePrompt.setEscapeButton(savePrompt.cancelButton)
-		savePrompt.exec()
-		if savePrompt.clickedButton() == savePrompt.cancelButton:
+		
+		dialog = ExitDialog("Changes to the file have not been saved!")
+		
+		dialog.exec()
+
+		if dialog.clickedButton() == dialog.cancelButton:
 			return event.ignore()
-		if savePrompt.clickedButton() == savePrompt.dontSaveButton:
+		if dialog.clickedButton() == dialog.dontSaveButton:
 			self.close()
 			return super().closeEvent(event)
-		if savePrompt.clickedButton() == savePrompt.saveButton:
+		if dialog.clickedButton() == dialog.saveButton:
 			if self.TextArea.toPlainText() != self.textBuffer[0] and self.textBuffer[1] == False:
 				ret = self.SaveFileAs()
 				if ret == False: return event.ignore()
@@ -125,6 +280,15 @@ class MainWindow(QWidget):
 			self.SaveFile()
 			self.close()
 			return super().closeEvent(event)
+	
+
+	@Slot()
+	def triggerClose(self):
+		self.close()
+	
+	@Slot()
+	def triggerMin(self):
+		self.showMinimized()
 	
 
 	
@@ -145,6 +309,24 @@ class MainWindow(QWidget):
 	
 	@Slot()
 	def OpenFile(self):
+
+		if self.TextArea.toPlainText() != self.textBuffer[0]:
+			
+			dialog = ExitDialog("You have unsaved changes!")
+		
+			dialog.exec()
+
+			if dialog.clickedButton() == dialog.cancelButton:
+				return
+			if dialog.clickedButton() == dialog.dontSaveButton:
+				pass
+			if dialog.clickedButton() == dialog.saveButton:
+				if self.textBuffer[1] == False:
+					ret = self.SaveFileAs()
+					if ret == False: return
+					pass
+				self.SaveFile()
+
 		f = QFileDialog.getOpenFileName(self, "Open a file", filter="Text Files (*.txt);;All Files (*.*)")[0]
 		if f == "": return
 
@@ -160,8 +342,8 @@ class MainWindow(QWidget):
 	def CheckSaveBuffer(self):
 		if self.textBuffer[1] == False: return
 		
-		if self.TextArea.toPlainText() != self.textBuffer[0]: self.menuBar.FileMenu.SaveAction.setDisabled(False)
-		elif self.TextArea.toPlainText() == self.textBuffer[0]: self.menuBar.FileMenu.SaveAction.setDisabled(True)
+		if self.TextArea.toPlainText() != self.textBuffer[0]: self.menuBar.FileMenu.SaveFileAction.setDisabled(False)
+		elif self.TextArea.toPlainText() == self.textBuffer[0]: self.menuBar.FileMenu.SaveFileAction.setDisabled(True)
 	
 	@Slot()
 	def SaveFile(self):
@@ -171,7 +353,7 @@ class MainWindow(QWidget):
 			content = self.TextArea.toPlainText()
 			fl.write(content)
 			self.textBuffer[0] = content
-			self.FileMenu.SaveAction.setDisabled(True)
+			self.menuBar.FileMenu.SaveFileAction.setDisabled(True)
 	
 	@Slot()
 	def SaveFileAs(self):
@@ -182,7 +364,7 @@ class MainWindow(QWidget):
 			content = self.TextArea.toPlainText()
 			fl.write(content)
 			self.textBuffer = [content, True]
-			self.curFile = f[0]
+			self.curFile = f
 			self.curFileDisplay.setText(self.curFile)
 			self.menuBar.FileMenu.CloseFileAction.setDisabled(False)
 	
@@ -193,43 +375,16 @@ class MainWindow(QWidget):
 		self.TextArea.setText("")
 		self.curFileDisplay.setText("No File opened")
 		self.menuBar.FileMenu.CloseFileAction.setDisabled(True)
+		self.menuBar.FileMenu.SaveFileAction.setDisabled(True)
 	
 
 
 	@Slot()
 	def changeFont(self):
-		font = QFontDialog.getFont(self)
+		font = QFontDialog.getFont()
 		if font[0] == False: return
 		self.TextArea.setFont(font[1])
-		data = ["font", font[1]]
-		UpdateJsonFile(self.settingsData[1], data)
-	
-	@Slot()
-	def changeColor(self):
-		color = QColorDialog(self).getColor()
-		if color.isValid() == False: return
-		cursor = self.TextArea.textCursor()
-		initPos = cursor.position()
-		cursor.movePosition(cursor.Start, cursor.MoveAnchor)
-		cursor.movePosition(cursor.End, cursor.KeepAnchor)
-		fg = QTextCharFormat()
-		fg.setForeground(QBrush(color))
-		cursor.setCharFormat(fg)
-		cursor.clearSelection()
-		self.color_ = color.name()
-		self.TextArea.setStyleSheet(f"color: {self.color_};background-color: {self.colorBG_};")
-		cursor.setPosition(initPos)
-		data = ["color", color.name()]
-		UpdateJsonFile(self.settingsData[1], data)
-	
-	@Slot()
-	def changeBGColor(self):
-		color = QColorDialog(self).getColor()
-		if color.isValid() == False: return
-		self.colorBG_ = color.name()
-		self.TextArea.setStyleSheet(f"color: {self.color_};background-color: {self.colorBG_};")
-		data = ["background", color.name()]
-		UpdateJsonFile(self.settingsData[1], data)
+		UpdateJsonFile(self.settingsData[1], font[1])
 	
 
 
@@ -239,7 +394,7 @@ def ReadJsonFile():
 	try:
 		with open("settings.json", "r") as f:
 			data = json.load(f)
-			if "font" not in data.keys() or "color" not in data.keys() or "background" not in data.keys(): return False
+			if "font" not in data.keys(): return False
 			else: return [data, os.path.realpath(f.name)]
 	except:
 		return False
@@ -247,50 +402,20 @@ def ReadJsonFile():
 def CreateJsonFile():
 	with open("settings.json", "w") as f:
 		data = {
-			"font": "Arial,16,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Standard",
-			"color": "#000000",
-			"background": "#FFFFFF"
+			"font": "Arial,16,-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Standard"
 		}
 		json.dump(data, f, indent=4)
 		return [data, os.path.realpath(f.name)]
 
 def UpdateJsonFile(f, ctx):
-	if ctx[0] == "font":
-		with open(f, "r") as fl:
-			try:
-				data = json.load(fl)
-			except:
-				CreateJsonFile()
-				return
-		
-		data["font"] = ctx[1].toString()
-
-		with open(f, "w") as fl:
-			json.dump(data, fl, indent=4)
-
-
-	if ctx[0] == "color":
-		with open(f, "r") as fl:
-			try:
-				data = json.load(fl)
-			except:
-				CreateJsonFile()
-				return
-		
-		data["color"] = ctx[1]
-
-		with open(f, "w") as fl:
-			json.dump(data, fl, indent=4)
+	with open(f, "r") as fl:
+		try:
+			data = json.load(fl)
+		except:
+			CreateJsonFile()
+			return
 	
-	if ctx[0] == "background":
-		with open(f, "r") as fl:
-			try:
-				data = json.load(fl)
-			except:
-				CreateJsonFile()
-				return
-		
-		data["background"] = ctx[1]
+	data["font"] = ctx.toString()
 
-		with open(f, "w") as fl:
-			json.dump(data, fl, indent=4)
+	with open(f, "w") as fl:
+		json.dump(data, fl, indent=4)
